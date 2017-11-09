f1fda8c447ef923f7625257142815c49c71e2594
FinalFreedom <u1664298@unimail.hud.ac.uk>
2017-11-09 09:20:45 +0000

    MahoganyTable
    With terrain mods

--- War/War/Assets/Scripts/Map/TileBehaviours.cs
+++ War/War/Assets/Scripts/Map/TileBehaviours.cs
@@ -2,30 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TileBehaviours : MonoBehaviour {
+public class TileBehaviours : MonoBehaviour
+{
     public Camera cam;
     public bool destructable = false;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
-	}
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            
+
             Ray ray = cam.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
-                if(hit.transform.gameObject.tag == "tile" && destructable)
+                if (hit.transform.gameObject.tag == "tile" && destructable)
                 {
                     Destroy(hit.transform.gameObject, 0f);
                 }
             }
         }
     }
-    
+
 }
